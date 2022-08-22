a50ae12d5de39098cd4b749362789d5c89c01c20
jiyoung <ji9310@naver.com>
2022-08-22 22:26:54 +0900

    Activity에서 viewModel 가져오기

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -3,28 +3,24 @@ package com.example.pokedex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pokedex.core.data.model.PokemonName
 import com.example.pokedex.feature.main.MainScreen
+import com.example.pokedex.feature.main.MainViewModel
 import com.example.pokedex.ui.theme.PokedexTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val mainViewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PokedexTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background,
-                ) {
-                    MainScreen(fakelist)
-                }
+                MainScreen(mainViewModel)
             }
         }
     }
@@ -41,6 +37,6 @@ private val fakelist = listOf(
 @Composable
 fun DefaultPreview() {
     PokedexTheme {
-        MainScreen(fakelist)
+
     }
 }
