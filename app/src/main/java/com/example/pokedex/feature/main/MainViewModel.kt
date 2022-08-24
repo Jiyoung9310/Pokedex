8343ac7029507208a38eb4361f322f30aa57810c
jiyoung <ji9310@naver.com>
2022-08-24 23:09:18 +0900

    다음 페이지 데이터 호출하기

--- app/src/main/java/com/example/pokedex/feature/main/MainViewModel.kt
+++ app/src/main/java/com/example/pokedex/feature/main/MainViewModel.kt
@@ -4,11 +4,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.pokedex.core.data.model.PokemonName
 import com.example.pokedex.core.data.repository.MainRepository
+import com.example.pokedex.core.network.model.NetworkPokemonListResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,17 +15,50 @@ class MainViewModel @Inject constructor(
     private val mainRepository: MainRepository,
 ): ViewModel() {
 
-    val feedState : StateFlow<PokemonNameUiState> = mainRepository.getPokemonNames()
-        .map { pokemonData ->
+    private var nextUrl : String? = null
+    private val feedFullList = mutableListOf<PokemonName>()
+
+    private val _feedState: MutableStateFlow<PokemonNameUiState> = MutableStateFlow(PokemonNameUiState.Loading)
+    val feedState : StateFlow<PokemonNameUiState> = _feedState
+
+    init {
+        getPokemonList()
+    }
+
+    private fun getPokemonList() {
+        viewModelScope.launch {
+            mainRepository.getPokemonNames()
+                .mapToFeedState()
+                .collect {
+                    _feedState.value = it
+                }
+        }
+    }
+
+    fun pageLoadMore() {
+        nextUrl?.let {
+            viewModelScope.launch {
+                mainRepository.getPokemonNamesMore(it)
+                    .mapToFeedState()
+                    .collect {
+                        _feedState.value = it
+                    }
+
+            }
+        }
+    }
+
+    private fun Flow<NetworkPokemonListResponse>.mapToFeedState(): Flow<PokemonNameUiState> =
+        map { pokemonData ->
+            nextUrl = pokemonData.next
+
             PokemonNameUiState.Success(
-                pokemonData.results.map {
+                feedFullList + pokemonData.results.map {
                     PokemonName(it.name)
                 }
             )
+        }.onEach {
+            feedFullList.clear()
+            feedFullList.addAll(it.feed)
         }
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(5_000),
-            initialValue = PokemonNameUiState.Loading
-        )
 }
