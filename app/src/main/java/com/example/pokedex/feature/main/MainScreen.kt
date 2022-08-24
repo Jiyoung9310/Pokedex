7144e95f00cc2376be4f9f195f980c31a097a495
jiyoung <ji9310@naver.com>
2022-08-24 23:11:06 +0900

    LoadMore 무한 스크롤 구현

--- app/src/main/java/com/example/pokedex/feature/main/MainScreen.kt
+++ app/src/main/java/com/example/pokedex/feature/main/MainScreen.kt
@@ -4,19 +4,20 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.pokedex.ui.theme.Pink80
+import kotlinx.coroutines.flow.distinctUntilChanged
 
 @Composable
 fun MainScreen(viewModel: MainViewModel) {
@@ -32,7 +33,10 @@ fun MainScreen(viewModel: MainViewModel) {
                 FeedLoading()
             }
             is PokemonNameUiState.Success -> {
-                PokemonFeed(feedState as PokemonNameUiState.Success)
+                PokemonFeed(
+                    (feedState as PokemonNameUiState.Success),
+                    viewModel::pageLoadMore
+                )
             }
         }
 
@@ -50,9 +54,13 @@ fun FeedLoading(
 @Composable
 fun PokemonFeed(
     feedState: PokemonNameUiState.Success,
+    loadMore : () -> Unit,
 ) {
-    LazyColumn {
-        items(feedState.feed) { item ->
+
+    val listState = rememberLazyListState()
+
+    LazyColumn(state = listState) {
+        items(feedState.feed, key = { it.name }) { item ->
             Card (
                 modifier = Modifier
                     .fillMaxSize()
@@ -67,5 +75,42 @@ fun PokemonFeed(
 
             }
         }
+    }
+
+    listState.OnBottomReached {
+        // do on load more
+        loadMore()
+    }
+}
+
+@Composable
+fun LazyListState.OnBottomReached(
+    buffer : Int = 2,
+    loadMore : () -> Unit,
+){
+    // state object which tells us if we should load more
+    val shouldLoadMore = remember {
+        derivedStateOf {
+            // get last visible item
+            val lastVisibleItem = layoutInfo.visibleItemsInfo.lastOrNull()
+                ?:
+                // list is empty
+                // return false here if loadMore should not be invoked if the list is empty
+                return@derivedStateOf true
+
+            // Check if last visible item is the last item in the list
+            lastVisibleItem.index == layoutInfo.totalItemsCount - buffer
+        }
+
+    }
+
+    // Convert the state into a cold flow and collect
+    LaunchedEffect(shouldLoadMore){
+        snapshotFlow { shouldLoadMore.value }
+            .distinctUntilChanged()
+            .collect {
+                // if should load more, then invoke loadMore
+                if (it) loadMore()
+            }
     }
 }
